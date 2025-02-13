990d2a4ad2889e070ceee98fabe2afab15c05d0e
Jem Davies <jemsot@gmail.com>
2025-02-13 14:41:28 +0000

    add output DLQ checks
    Signed-off-by: Jem Davies <jemsot@gmail.com>

--- internal/bundle/strict/package.go
+++ internal/bundle/strict/package.go
@@ -3,6 +3,8 @@ package strict
 import "slices"
 
 var incompatibleProcessors []string
+var incompatibleOutputs []string
+var parentProcessors []string
 
 func init() {
 	incompatibleProcessors = []string{
@@ -10,9 +12,35 @@ func init() {
 		"catch",
 		"switch",
 	}
+
+	incompatibleOutputs = []string{
+		"reject_errored",
+		"switch",
+	}
+
+	parentProcessors = []string{
+		"branch",
+		"cached",
+		"catch",
+		"for_each",
+		"group_by",
+		"parallel",
+		"retry",
+		"switch",
+		"try",
+		"while",
+		"workflow",
+	}
 }
 
 func isProcessorIncompatible(name string) bool {
 	return slices.Contains(incompatibleProcessors, name)
+}
 
+func isOutputIncompatible(name string) bool {
+	return slices.Contains(incompatibleOutputs, name)
+}
+
+func isParentProcessor(name string) bool {
+	return slices.Contains(parentProcessors, name)
 }
