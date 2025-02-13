8c2ae12145645f3f621f160168e012bbd1518f93
Jem Davies <jemsot@gmail.com>
2025-02-13 16:47:09 +0000

    add test for DLQ disable strict mode
    Signed-off-by: Jem Davies <jemsot@gmail.com>

--- internal/bundle/strict/package.go
+++ internal/bundle/strict/package.go
@@ -4,32 +4,18 @@ import "slices"
 
 var incompatibleProcessors []string
 var incompatibleOutputs []string
-var parentProcessors []string
 
 func init() {
 	incompatibleProcessors = []string{
 		"try",
 		"catch",
 		"switch",
+		"retry",
 	}
 
 	incompatibleOutputs = []string{
 		"reject_errored",
 		"switch",
-	}
-
-	parentProcessors = []string{
-		"branch",
-		"cached",
-		"catch",
-		"for_each",
-		"group_by",
-		"parallel",
-		"retry",
-		"switch",
-		"try",
-		"while",
-		"workflow",
 	}
 }
 
@@ -39,8 +25,4 @@ func isProcessorIncompatible(name string) bool {
 
 func isOutputIncompatible(name string) bool {
 	return slices.Contains(incompatibleOutputs, name)
-}
-
-func isParentProcessor(name string) bool {
-	return slices.Contains(parentProcessors, name)
 }
